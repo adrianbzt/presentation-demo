13db9432272188cfae4c0e11f8315e9a3dfde085
Adrian Buzatu <adrian.buzatu@gameloft.com>
2018-04-26 14:29:33 +0300

    auth

--- src/App.js
+++ src/App.js
@@ -2,16 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase';
+import firebaseui from 'firebaseui';
+
+
+
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyBK4Z-4SivhPTPmfEeOASMVYG0K3p073mU",
+    authDomain: "presentation-demo-9ef83.firebaseapp.com",
+    databaseURL: "https://presentation-demo-9ef83.firebaseio.com",
+    projectId: "presentation-demo-9ef83",
+    storageBucket: "presentation-demo-9ef83.appspot.com",
+    messagingSenderId: "821628220371"
+  };
+  firebase.initializeApp(config);
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());  
+
 class App extends Component {
 
   doLogin() {
-    alert('I have to login');
+	  ui.start('#firebaseui-auth-container', {
+		  signInOptions: [
+			  firebase.auth.EmailAuthProvider.PROVIDER_ID,
+			  firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+			  firebase.auth.PhoneAuthProvider.PROVIDER_ID
+		  ],
+	  });
   }
   render() {
     return (
       <div className="App">
-        <h1>Hello, World!</h1>
-        <button onClick={this.doLogin}>Login</button>
+        
+
+      <h1>Welcome to My Awesome App</h1>
+      <div id="firebaseui-auth-container"></div>
+      <button onClick={this.doLogin}>Login</button>
+
       </div>
     );
   }
