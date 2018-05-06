05ca9212660235b7fe4c4e1c58c9fa0de4c4840f
Adrian <adrianbuzatu86@gmail.com>
2018-05-06 15:00:45 +0300

    work

--- src/layout/default.js
+++ src/layout/default.js
@@ -1,21 +1,22 @@
 import React, {Component} from 'react';
 import Navigation from './navigation';
+import Footer from './footer';
 
 class Layout extends Component {
     render() {
         return(
-            <div className="container">
-            <div className="header">
-                <Navigation />
-            </div>
-            <div className="main">
-            <h1> Aici e un main <br/>
-                React + Bootstrap = &hearts;
-            </h1>
-            </div>
-            <div className="footer">
-            <h1> Aici e un footer</h1>
-            </div>
+            <div className="container-fluid">
+                <div className="header">
+                    <Navigation />
+                </div>
+                <div className="main">
+                <h1> Aici e un main <br/>
+                    React + Bootstrap = &hearts;
+                </h1>
+                </div>
+                <div className="footer">
+                    <Footer />
+                </div>
             </div>
         );
     }
